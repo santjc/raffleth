90f01d992c178ab72093c6d033851b9f31dc0fe1
santjc <sjcolombatto@hotmail.com>
2022-04-01 17:40:05 -0300

    Hardhat/Web3 dependencies, wallet connection

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppBar, Button, Container, Box, Toolbar } from "@mui/material";
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import { LogoDev } from "@mui/icons-material/";
-import "./navbar.scss"
+import "./navbar.scss";
 
 const pages = ["About", "Contact", "Get Started"];
 const settings = ["Profile", "My Raffles"];
+
 const Navbar = () => {
+  const [currentAccount, setAccount] = useState(null);
+  const checkWalletConnection = () => {
+    const ethereum = window.ethereum;
+    if (ethereum) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const connectWalletHandler = async () => {
+    if (checkWalletConnection()) {
+      try {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    }else{
+      alert("Metamask isn't installed");
+    }
+  };
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
   return (
     <AppBar id="navbar" position="relative">
+      {currentAccount != null && (
+        <div className="wallet-status">Wallet Connected</div>
+      )}
       <Container maxWidth="xl">
         <Toolbar>
-        <Button key="logo" sx={{ my: 1, color: "white", fontWeight: "bold", fontSize:"22px"}}>
-                <LogoDev></LogoDev>
-        </Button>
+          <Button
+            key="logo"
+            sx={{ my: 1, color: "white", fontWeight: "bold", fontSize: "22px" }}
+          >
+            <LogoDev></LogoDev>
+          </Button>
           <Box
             sx={{
               justifyContent: "end",
@@ -24,13 +57,27 @@ const Navbar = () => {
             {pages.map((page) => (
               <Button
                 key={page}
-                sx={{ my: 1, color: "white", fontWeight: "normal", fontSize:"16px"}}
+                sx={{
+                  my: 1,
+                  color: "white",
+                  fontWeight: "normal",
+                  fontSize: "16px",
+                }}
               >
                 {page}
               </Button>
             ))}
-            <Button key="account" sx={{ my: 1, color: "white", fontWeight: "bold", fontSize:"18px"}}>
-                <AccountCircle></AccountCircle>
+            <Button
+              onClick={() => connectWalletHandler()}
+              key="account"
+              sx={{
+                my: 1,
+                color: "white",
+                fontWeight: "bold",
+                fontSize: "18px",
+              }}
+            >
+              <AccountCircle></AccountCircle>
             </Button>
           </Box>
         </Toolbar>
---
+++ src/components/Wallet/Wallet.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Wallet = () => {
+    
+}
+
+export default Wallet;
